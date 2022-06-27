0851f0acfe55acb59b6ab993fa3b227a6e50be9d
Shivani Bhakta <shivanibhakta@Shivanis-MacBook-Pro.local>
2022-06-27 15:12:35 -0700

    lab1 added

---
+++ labs/lab1/tests/q5_4_1.py
@@ -0,0 +1,1 @@
+test = {'name': 'q5_4_1', 'points': None, 'suites': [{'cases': [{'code': '>>> sentence_length == 896\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
