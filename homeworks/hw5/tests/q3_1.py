635997300796698737fbd4a876ae9ef53326b632
bhaktashivani <s9bhakta@ucsd.edu>
2022-08-05 13:26:05 -0700

    hw5 published

---
+++ homeworks/hw5/tests/q3_1.py
@@ -0,0 +1,11 @@
+test = {   'name': 'q3_1',
+    'points': None,
+    'suites': [   {   'cases': [   {   'code': '>>> callable(mean_diff) and np.isclose(mean_diff(covid_politics), difference_by_residents) # When the function is called on input covid_politics,the '
+                                               'output should be the same as the variable difference_by_residents\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ homeworks/hw5/tests/q3_2.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q3_2',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> callable(pick_15) and isinstance(pick_15(), bpd.DataFrame)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> pick_15().shape == (15, 3)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
